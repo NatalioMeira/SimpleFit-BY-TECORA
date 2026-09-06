cf118d71485fbeed4984eb6487fcf1a76c27e93f
stein <tekc8957@gmail.com>
2026-09-06 14:30:41 -0300

    Refactor IA client variable and enhance comments
    Refactor variable naming and improve comments for clarity.

--- personal-escola/ia.py
+++ personal-escola/ia.py
@@ -1,14 +1,17 @@
 import os
 from dotenv import load_dotenv
-
+#      ^ nessa linha eu importo a funcao de carregar um arquivo .env, que contem a API para a ia da groq.
 load_dotenv()
-
+#        ↓ transforma a chave numa variavel pra ficar mais facil de usar.
 CHAVE_API = os.getenv("GROQ_API_KEY") or os.getenv("gateway")
 
-_cliente_ia = None
+cliente_ia = None
+#    ^ forca a criacao de uma variavel sem valor.
+#    ↓ verfica se a chave existe, e se sim ele define a variavel vazia para a api.
 if CHAVE_API:
+#           ↓ importei dentro do if para caso o cliente nao possua uma chave, ele nao importe a biblioteca para nao depender dela.
     from groq import Groq
-    _cliente_ia = Groq(api_key=CHAVE_API)
+    cliente_ia = Groq(api_key=CHAVE_API)
 
 
 def falar(mensagem, pessoa):
@@ -48,22 +51,30 @@ médicas.
 Diga os treinos que a pessoa deve fazer e em que horario de que dia,
 baseado no contexto dado.
 
-Seja muito breve dizendo apenas o necessario.
+Seja muito breve, dizendo apenas o necessário.
 
 Mensagem:
 {mensagem}
 """
 
     try:
-        resposta = _cliente_ia.chat.completions.create(
+#                    ↓ simplifica a resposta da ia pra apenas "resposta".
+        resposta = cliente_ia.chat.completions.create(
             model="openai/gpt-oss-120b",
+#                    ^ modelo da IA.
             messages=[{"role": "user", "content": prompt}],
+#                        ^ mostra quem mandou a mensagem e manda o prompt padrao para ele mostrar a dieta e a rotina de treino.
             temperature=0.7,
+#                ^ define o quao criativo a ia pode ser (nesse caso ta como meio termo).
+            
         )
+#                          ↓ e uma lista de possiveis respostas geradas pela ia ( o 0 significa que e a primeira).
         return resposta.choices[0].message.content
+#                                    ^ pega o conteudo da resposta.
 
     except Exception as erro:
         return "Erro na IA: " + str(erro)
+#                    ^ em caso de erros, nao fazer o sistema todo parar.
 
 
 def dieta(pessoa):
