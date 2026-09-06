124a89f1cf8d9c3d7341ebaa169c1c8ac8e833ff
stein <tekc8957@gmail.com>
2026-09-06 01:06:46 -0300

    Refactor email regex and simplify to_dict method

--- personal-escola/ClassCadastro.py
+++ personal-escola/ClassCadastro.py
@@ -1,15 +1,15 @@
 import hashlib
+# essa biblioteca transforma o dado em um valor fixo e praticamente nao da pra reverter para descobrir o original.
 import re
-
+# re e usada para procurar, validar e manipular padroes dentro de textos usando expressoes (que seria o regex).
 from database import adicionar_usuario, buscar_usuario_por_email
 
-REGEX_EMAIL = re.compile(r"^[^@\s]+@[^@\s]+\.[^@\s]+$")
-
-
+regex_email = re.compile(r"^[^@\s]+@[^@\s]+\.[^@\s]+$")
+# ^ ta verificando se o email foi escrito de forma correta, ex: teste gmail.com nao daria certo mas teste@gmail.com daria.
+# ↓  transformando a senha em hash (copiei a seta do google pois nao achei uma seta pra usar).
 def hash_senha(senha: str) -> str:
     return hashlib.sha256(senha.encode("utf-8")).hexdigest()
-
-
+#                            ↓ o none serve pra nao precisar de credenciais pra chamar um metodo.
 class Usuario:
     def __init__(self, nome=None, contato=None, email=None, idade=None, senha=None):
         self.nome = nome
@@ -17,24 +17,19 @@ class Usuario:
         self.email = email
         self.idade = idade
         self.senha = senha 
-
+        
+#        ↓ to_dict serve pra transformar valores em um valor de dicionario (dict) que depois vou colocar em um json.
     def to_dict(self):
-        return {
-            "nome": self.nome,
-            "contato": self.contato,
-            "email": self.email,
-            "idade": self.idade,
-            "senha": self.senha,
-        }
-
+        return {"nome": self.nome, "contato": self.contato, "email": self.email, "idade": self.idade, "senha": self.senha}
+#        ↓ auto explicativo.
     def cadastro(self):
         print("\n===== CADASTRO =====")
-
+#                                                 ↓ serve para tirar os espacos e quebras de linha na parte de fora do valor.
         self.nome = input("Seu nome completo: ").strip()
 
         while True:
             email = input("Email: ").strip()
-            if not REGEX_EMAIL.match(email):
+            if not regex_email.match(email):
                 print("Email inválido. Tente novamente.")
                 continue
             if buscar_usuario_por_email(email):
@@ -44,7 +39,7 @@ class Usuario:
             break
 
         self.contato = input("Contato: ").strip()
-
+#                        ^ definimos como string pois nem todo contato e numerico e mesmo que seja ele seria considerado.
         while True:
             try:
                 self.idade = int(input("Idade: "))
